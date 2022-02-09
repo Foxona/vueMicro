15b900b4f1bcb07e28ea96fda608138d7656b297
pawfen <pawfen>
2022-02-09 16:00:43 +0300

    vue test 2 components

---
+++ src/components/Timer.vue
@@ -0,0 +1,47 @@
+<template>
+  <button @click="increment">Кнопка нажата {{ clickCount }} раз</button>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+const clickCount = ref(0);
+
+function increment() {
+  clickCount.value++;
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${clickCount.value}.`);
+});
+
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <input v-model="newTodo" @keyup.enter="addTodo" />
+  <button @click="addTodo">Add Todo</button>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let id = 0;
+
+const newTodo = ref("");
+const todos = ref([
+  { id: id++, text: "Learn HTML" },
+  { id: id++, text: "Learn JavaScript" },
+  { id: id++, text: "Learn Vue" },
+]);
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value });
+  newTodo.value = "";
+}
+
+function removeTodo(todo) {
+  console.log(todo);
+  todos.value = todos.value.filter((task) => task !== todo);
+}
+</script>
+
+<style scoped>
+</style>
